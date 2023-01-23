1c92cde238fec709d1c030e45d5c5d73d66164e2
Mahammadali Huseynov <93779611+MahammadAli98@users.noreply.github.com>
2023-01-23 11:46:09 +0400

    Update server.mjs

--- public/server.mjs
+++ public/server.mjs
@@ -5,29 +5,26 @@ const app = express()
 const server = createServer(app)
 const clients = {}
 
-const wss = new WebSocketServer({ server: server })
+const wss = new WebSocketServer({ server })
 
 const getUniqueID = () => {
     const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
     return s4() + s4() + '-' + s4()
 }
 
-wss.on('connection', function connection(ws) {
-
+wss.on('connection', (ws) => {
     let userID = getUniqueID()
-    console.log((new Date()) + ' Recieved a new connection from origin ' + ws.origin + '.')
-
     clients[userID] = ws
-    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
-    console.log('A new client Connected! ' + userID)
+    console.log((new Date()) + ' WS origin: ' + userID)
 
     ws.send('Welcome New Client!')
 
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message)
-        wss.clients.forEach(function each(client) { if (client !== ws && client.readyState === WebSocket.OPEN) client.send(message) })
+    ws.on('message', (data) => {
+        console.log('received:', JSON.parse(data).message)
+        wss.clients.forEach((ws) => ws.send(JSON.parse(data).message))
+        // wss.clients.forEach(function each(client) { if (client !== ws && client.readyState === WebSocket.OPEN) client.send(message) })
     })
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
-server.listen(5000, () => console.log(`Lisening on port :5000`))
+app.get('/', (req, res) => res.send('Server is up!'))
+server.listen(5000, '0.0.0.0', () => console.log(`Lisening on port: 5000`))
